9ad262f612648ba5cd6f46a78228c6e7f32d0b40
thanhtung0088 <63912252+thanhtung0088@users.noreply.github.com>
2025-12-25 20:01:52 +0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Sidebar from './components/Sidebar';
+import Sidebar from './Sidebar';
 import AIChatbox from './components/AIChatbox';
 import Dashboard from './pages/Dashboard';
 import LessonPlanner from './pages/LessonPlanner';
