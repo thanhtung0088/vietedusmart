c6e2689993ba4b5b12dfc5908de6d13362eef42d
thanhtung0088 <63912252+thanhtung0088@users.noreply.github.com>
2025-12-25 16:40:03 +0700

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,15 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    // Cho phép ứng dụng truy cập API_KEY từ biến môi trường của Vercel
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
+  },
+  server: {
+    port: 3000
+  },
   build: {
     outDir: 'dist',
-    target: 'esnext'
+    sourcemap: false
   }
 });
