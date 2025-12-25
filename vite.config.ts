6b00452326279dbf8960e26aaaf8d5bdaa8d65be
thanhtung0088 <63912252+thanhtung0088@users.noreply.github.com>
2025-12-25 18:13:36 +0700

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
+    extensions: ['.tsx', '.ts', '.jsx', '.js']
   },
   define: {
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
